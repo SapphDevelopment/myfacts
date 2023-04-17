92365dcfc97c751ed31e2b03a4dab5f8bc4d19d3
Prits <36541603+Prits001@users.noreply.github.com>
2023-04-18 02:58:08 +0300

    Update imports.js

--- src/facts/imports.js
+++ src/facts/imports.js
@@ -1,4 +1,5 @@
 import { discordFacts } from "./discord.js";
+import { dogFacts } from "./dog.js";
 import { googleFacts } from "./google.js";
 import { openaiFacts } from "./openai.js";
 import { softwareFacts } from "./software.js";
@@ -7,6 +8,7 @@ import { youtubeFacts } from "./youtube.js";
 
 export {
   discordFacts,
+  dogFacts,
   googleFacts,
   openaiFacts,
   softwareFacts,
